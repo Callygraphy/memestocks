75faff5e7c6be73a705531a009b101ebaac81df3
Cally Gatehouse <Cally.Gatehouse@gmail.com>
2013-04-16 16:07:29 +0100

    changed to meme generator, added views, routed to edition and sample

--- web.js
+++ web.js
@@ -1,34 +1,44 @@
-var express = require('express');
+var express = require('express')
+  , path = require('path');
+
+
 var restler = require('restler');
 
 var app = express.createServer(express.logger());
 
-app.all('/', function(request, response) {
-	restler.get('http://www.reddit.com/r/AdviceAnimals/.json').on('complete', function(reddit) {
-		var titles = "<body style=\"width: 384px; padding: 0px; margin: 0px;\"><h1 style=\"font-size: 40px;\">MEME STOCKS<h1/><h2>When to use which MEME</h2>";
-		for(var i=0; i<1; i++) {
-			var imageURL = reddit.data.children[i].data.url; 
-			if (imageURL.indexOf(".jpg")!= -1 || imageURL.indexOf(".jpeg")!= -1 ) {
-				titles += "<img src=\"" + reddit.data.children[i].data.url + "\" />";
-			} else if (imageURL.indexOf("qkme.me")!= -1 || imageURL.indexOf("quickmeme")!= -1) {
-				var myRe = /\/([\d\w]{6})/; //finds 6 digit alphanumerical code preceded by a slash
-				var newURL = myRe.exec(imageURL);
-				if (newURL == null) {
-				    titles += "<p> no" + imageURL + "match</p>";
-				} else {
-    				titles += "<img src=\"http://i.qkme.me/" + newURL[1] + ".jpg\" width=384px/>";
-  				}
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set("view options", { layout: false })
+app.use(express.static(path.join(__dirname, 'public')));
 
-			} else {
-				titles += "<p>Oops. something broke!</p>";}
-		}
+
+app.get('/edition/', function(request, response) {
+	restler.get('http://version1.api.memegenerator.net/Generators_Select_ByPopular?pageIndex=0&pageSize=12&days=7').on('complete', function(reddit) {
+		var advice = "Use <em>" + reddit.result[1].displayName + "</em> with caution."
+		var memePic = reddit.result[0].imageUrl;
+		var topOfThePops = "";
 		for(var i=0; i<5; i++) {
-			titles += "<p>" + reddit.data.children[i].data.title + "</p>";
+			topOfThePops += "<li>" + reddit.result[i].displayName + "</li>";
 		}
-		titles += "</body>";
-		response.send(titles);
+		response.render('index', { memePic: memePic, topOfThePops: topOfThePops, advice: advice });
+
 	});
 });
+
+app.get('/sample/', function(request, response) {
+	restler.get('http://version1.api.memegenerator.net/Generators_Select_ByPopular?pageIndex=0&pageSize=12&days=7').on('complete', function(reddit) {
+		var advice = "Use <em>" + reddit.result[1].displayName + "</em> with caution."
+		var memePic = reddit.result[0].imageUrl;
+		var topOfThePops = "";
+		for(var i=0; i<5; i++) {
+			topOfThePops += "<li>" + reddit.result[i].displayName + "</li>";
+		}
+		response.render('index', { memePic: memePic, topOfThePops: topOfThePops, advice: advice });
+
+	});
+});
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {

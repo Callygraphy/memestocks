dc60e54a7a87785c69936241c5f4323587da499c
Cally Gatehouse <Cally.Gatehouse@gmail.com>
2013-04-17 14:01:53 +0100

    Made a weekly publication, updated express version, added etags, added routes folder

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+
+/*
+ * GET home page.
+ */
+
+var restler = require('restler');
+var littleprinter = require('littleprinter')
+
+exports.index = function(request, response) {
+	var d = new Date();
+
+	var today = d.getDay();
+
+	if (today == 3) {
+
+		restler.get('http://version1.api.memegenerator.net/Generators_Select_ByPopular?pageIndex=0&pageSize=12&days=7').on('complete', function(reddit) {
+			var advice = "Use <strong>" + reddit.result[1].displayName + "</strong> with caution."
+			
+			var memePic = reddit.result[0].imageUrl;
+			
+			var topOfThePops = ""
+			for(var i=0; i<5; i++) {
+				topOfThePops += "<li>" + reddit.result[i].displayName + "</li>";}
+			
+			//var etag = littleprinter.createEtag(d.toString())
+		 	//response.set('Etag', etag);	 	
+			
+			response.render('index', { memePic: memePic, topOfThePops: topOfThePops, advice: advice });
+		});
+	} else { 
+			var etag = littleprinter.createEtag(d.toString())
+		 	response.set('Etag', etag);
+		 	
+		 	response.send(204);
+		 }
+};

26b9566e7f40c158c85522260c73248beec4207d
Cally Gatehouse <Cally.Gatehouse@gmail.com>
2013-04-25 10:41:16 +0100

    changed source of etag hash

--- routes/index.js
+++ routes/index.js
@@ -30,7 +30,7 @@ exports.index = function(request, response) {
 			for(var i=0; i<5; i++) {
 				topOfThePops += "<li><span class=\"text\">" + reddit.result[i].displayName + "</span></li>";}
 
-			var etag = littleprinter.createEtag(d.toString())
+			var etag = littleprinter.createEtag(d.toString() + "edition")
 		 	response.set('Etag', etag);
 		 	if(request.headers && request.headers['if-none-match'] === etag) {
      			return response.send(304);
@@ -39,7 +39,7 @@ exports.index = function(request, response) {
 			response.render('index', { memePic: memePic, topOfThePops: topOfThePops, advice: advice, date: date });
 		});
 	} else { 
-			var etag = littleprinter.createEtag(d.toString())
+			var etag = littleprinter.createEtag(d.toString() + "none")
 		 	response.set('Etag', etag);
 		 	if(request.headers && request.headers['if-none-match'] === etag) {
      			return response.send(304);
